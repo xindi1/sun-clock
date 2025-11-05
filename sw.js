b30cd8aa162be303ce6aa404434f7c6f30d18c14
xindi1 <robert.graham411@gmail.com>
2025-11-04 20:51:22 -0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,38 @@
+
+const CACHE_VERSION = 'sunclock-v2';
+const CORE_ASSETS = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './sw.js',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_VERSION).then(cache => cache.addAll(CORE_ASSETS)).then(self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(key => {
+      if (key !== CACHE_VERSION) return caches.delete(key);
+    }))).then(self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  const req = event.request;
+  // Cache-first for same-origin GETs
+  if (req.method === 'GET' && new URL(req.url).origin === location.origin) {
+    event.respondWith(
+      caches.match(req).then(cached => cached || fetch(req).then(res => {
+        const copy = res.clone();
+        caches.open(CACHE_VERSION).then(cache => cache.put(req, copy));
+        return res;
+      }).catch(() => caches.match('./index.html')))
+    );
+  }
+});
